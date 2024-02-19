f9e77b66fb6be4650d133269f1a0aa39aedec645
Haider Daij <haiderduaij@gmail.com>
2024-02-20 01:52:36 +0300

    Notify IP

--- src/components/Navbar/socketServer.js
+++ src/components/Navbar/socketServer.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
-const SOCKET_IO_URL = "http://localhost:3500";
+const IP_ADDRESS = "172.20.10.3"; //192.168.1.5
+const SOCKET_IO_URL = `http://${IP_ADDRESS}:3500`;
 const socket = io(SOCKET_IO_URL);
 
 export default socket;
